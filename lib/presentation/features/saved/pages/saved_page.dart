3221d0d3921a226c2a0b5f72ec6785a1b74cc2f4
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-09-05 11:22:42 +0700

    Add custom AppBar to SavedPage
    Replaces the default scaffold with a custom AppBar featuring a back button, styled title, and divider. Updates the body text for clarity and applies theme colors from app_colors.

--- lib/presentation/features/saved/pages/saved_page.dart
+++ lib/presentation/features/saved/pages/saved_page.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/core/theme/app_colors.dart';
 
 class SavedPage extends StatelessWidget {
   const SavedPage({super.key});
+
   @override
-  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text('Saved Recipes')));
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {},
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        title: Text(
+          'Công thức',
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+            color: PrimaryColors.shade700,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1.0),
+          child: Divider(
+            height: 1.0,
+            thickness: 1.0,
+            color: NeutralColors.shade300,
+          ),
+        ),
+      ),
+      body: const Center(child: Text('This is the Saved Page')),
+    );
+  }
 }
