00bb2a89d3548a32fa05bdb0e70f6008679b7955
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-09-05 11:06:58 +0700

    Enhance recipe detail page UI and navigation
    Added go_router for navigation and updated the SliverAppBar with a custom back button. Improved layout with ingredient and process buttons, detailed ingredient sections, and a video button for better user experience.

--- lib/presentation/features/recipe_detail/pages/recipe_detail_page.dart
+++ lib/presentation/features/recipe_detail/pages/recipe_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:recipe_app/core/theme/app_colors.dart';
 
@@ -8,14 +9,32 @@ class RecipeDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
             automaticallyImplyLeading: false,
-            expandedHeight: 300,
-            pinned: true,
-            title: Text('Chi tiết'),
             titleSpacing: 0,
+            expandedHeight: 350,
+            pinned: true,
+            title: InkWell(
+              onTap: () {
+                context.pop();
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.chevron_left, size: 24, color: Colors.black),
+
+                  Text(
+                    "Chi tiết",
+                    style: Theme.of(
+                      context,
+                    ).textTheme.bodyMedium?.copyWith(color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
             flexibleSpace: FlexibleSpaceBar(
               background: Image.asset(
                 'assets/images/image1.png',
@@ -23,6 +42,7 @@ class RecipeDetailPage extends StatelessWidget {
               ),
             ),
           ),
+
           SliverToBoxAdapter(
             child: Container(
               decoration: BoxDecoration(color: Colors.white),
@@ -145,6 +165,121 @@ class RecipeDetailPage extends StatelessWidget {
                       color: Theme.of(context).colorScheme.primary,
                       thickness: 2,
                     ),
+                    SizedBox(height: 24),
+                    Row(
+                      children: [
+                        SizedBox(width: 24),
+                        Expanded(
+                          child: FilledButton(
+                            onPressed: () {},
+                            style: FilledButton.styleFrom(
+                              backgroundColor: Theme.of(
+                                context,
+                              ).colorScheme.primary,
+                              padding: const EdgeInsets.symmetric(vertical: 12),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              "Nguyên liệu",
+                              style: Theme.of(context).textTheme.titleMedium
+                                  ?.copyWith(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 24),
+
+                        Expanded(
+                          child: TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(vertical: 12),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              "Chế biến",
+                              style: Theme.of(context).textTheme.titleMedium
+                                  ?.copyWith(
+                                    color: Theme.of(
+                                      context,
+                                    ).colorScheme.primary,
+                                  ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 24),
+                      ],
+                    ),
+                    SizedBox(height: 24),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Dành cho 2-4 người ăn',
+                            style: Theme.of(context).textTheme.bodyLarge
+                                ?.copyWith(fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 12),
+                          Text(
+                            '300g chân gà, luộc trong 20 phút\n2 chiếc xúc xích bò\n5 viên thịt bò\n100g bánh sắn, luộc cho đến khi mềm\n1 quả trứng đánh tan\n50 cải xanh, cắt thành 4 miếng',
+                            style: Theme.of(context).textTheme.bodyMedium
+                                ?.copyWith(color: NeutralColors.shade700),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 24),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Đối với bột gia vị',
+                            style: Theme.of(context).textTheme.bodyLarge
+                                ?.copyWith(fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 12),
+                          Text(
+                            '15g tỏi\n3g mắm tôm\n25g hành tím\n50g gừng thơm\n100g ớt đỏ lớn\n50g ớt cay\n7g muối\n15g đường\n15g hành tây chiên\n25g bột gà\n50ml dầu ăn',
+                            style: Theme.of(context).textTheme.bodyMedium
+                                ?.copyWith(color: NeutralColors.shade700),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 24),
+                    SizedBox(
+                      width: double.infinity,
+                      child: FilledButton.icon(
+                        onPressed: () {},
+                        style: FilledButton.styleFrom(
+                          backgroundColor: MiscellaneousColors.tinted
+                              .withValues(alpha: 0.15),
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        icon: Icon(
+                          Icons.live_tv,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        label: Text(
+                          "Xem video",
+                          style: Theme.of(context).textTheme.titleMedium
+                              ?.copyWith(
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 34),
                   ],
                 ),
               ),
